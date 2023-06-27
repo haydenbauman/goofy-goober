d862c62d9b1484e84afa59aa0dc13324b924cb2b
Hayden Bauman <hbaumans@icloud.com>
2023-06-27 21:13:59 +0000

    finished game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -47,19 +47,25 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
-    // TODO 2
+  createPlatform(1000, 250, 50, 20); // short but wide platform located 1000 pixels from the left of the screen and 100 pixels from the top of the screen
+  createPlatform(1000, 500, 200, 200); // short but wide platform located 1000 pixels from the left of the screen and 500 pixels from the top of the screen
+  createPlatform(100, 400, 500, 100); // short but wide platform located 100 pixels from the left of the screen and 400 pixels from the top of the screen
+  createPlatform(750, 600, 100, 50); // short but wide platform located 750 pixels from the left of the screen and 600 pixels from the top of the screen
+  createPlatform(200, 700, 400, 70); // short but wide platform located 200 pixels from the left of the screen and 700 pixels from the top of the screen
+  createPlatform(750, 450, 100, 50); // short but wide platform located 150 pixels from the left of the screen and 50 pixels from the top of the screen
+  createPlatform(1000, 450, 200, 200); // short but wide platform located 1000 pixels from the left of the screen and 100 pixels from the top of the screen
+  createPlatform(950, 525, 50, 20); // short but wide platform located 1000 pixels from the left of the screen and 100 pixels from the top of the screen
+  createPlatform(750, 300, 100, 50); // short but wide platform located 1000 pixels from the left of the screen and 100 pixels from the top of the screen
+  createPlatform(550, 450, 100, 50); // short but wide platform located 150 pixels from the left of the screen and 50 pixels from the top of the screen
+  // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
-
+  createCollectable("diamond", 1000, 190, 1, 1);
+  createCollectable("database", 150, 300, 1, 1);
+  createCollectable("kennedi", 200, 600, 1, 1);
+  createCollectable("grace", 600, 400, 1, 1);
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
